3b76385fd4c71c816ae304eaebecb33bc77b4fda
martinjrobins <martinjrobins@gmail.com>
2022-12-13 14:31:49 +0000

    update version

--- bpx/__init__.py
+++ bpx/__init__.py
@@ -1,7 +1,7 @@
 """BPX schema and parsers"""
 # flake8: noqa F401
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .interpolated_table import InterpolatedTable
 from .expression_parser import ExpressionParser
---
+++ docs/CONTRIBUTING.md
@@ -0,0 +1,4 @@
+## Making a release
+
+1. Update version number in `bpx/__init__.py`
+2. Create a tag on the commit you want to release, this will kick off the `publish_pypi.yml` workflow
