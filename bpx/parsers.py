da25bf4230f957ab0880590ec98ac0e977e3a12c
kratman <kratmandu@gmail.com>
2024-12-19 15:41:35 -0500

    Revert experiment

--- bpx/parsers.py
+++ bpx/parsers.py
@@ -26,7 +26,7 @@ def parse_bpx_obj(bpx: dict, v_tol: float = 0.001) -> BPX:
 
     BPX.Settings.tolerances["Voltage [V]"] = v_tol
 
-    return BPX.model_validate(bpx, strict=False)
+    return BPX.model_validate(bpx)
 
 
 def parse_bpx_file(filename: str | Path, v_tol: float = 0.001) -> BPX:
