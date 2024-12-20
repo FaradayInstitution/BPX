d9ad3aee11381eb95f03b1ffe270e4ada03f7ee2
kratman <kratmandu@gmail.com>
2024-12-20 09:49:26 -0500

    Attempt to get the new values

--- bpx/schema.py
+++ bpx/schema.py
@@ -319,10 +319,13 @@ class UserDefined(BaseModel):
                 data[k] = InterpolatedTable(**v)
         super().__init__(**data)
 
+    @property
+    def extra_fields(self) -> set[str]:
+        return set(self.__dict__) - set(self.model_fields)
+
     @model_validator(mode="before")
     @classmethod
     def validate_extra_fields(cls, values: dict) -> dict:
-        print(values)
         for k, v in values.items():
             if not isinstance(v, get_args(FloatFunctionTable)):
                 error_msg = f"{k} must be of type 'FloatFunctionTable'"
